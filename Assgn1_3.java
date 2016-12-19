c1889abaeec178da6e5be3475aeebc713392c24e
karthikudupa123 <karthikudupa123@gmail.com>
2016-12-19 11:06:35 +0530

    commit

---
+++ Assgn1_3.java
@@ -0,0 +1,12 @@
+public class Assgn1_3
+{
+ public static void main(String [] args)
+ {
+  int a=20,b=10;
+  System.out.println("a="+a+" b="+b);
+  a=a+b;
+  b=a-b;
+  a=a-b;
+  System.out.println("a="+a+" b="+b);
+  }
+}
